d0c32ac404c3242473963eedb62cb80ab09bef19
Tillson Galloway <tillson@users.noreply.github.com>
2015-10-04 00:08:16 -0400

    Update control.sh

--- terminal/control.sh
+++ terminal/control.sh
@@ -1,1 +1,1 @@
-defaults write com.apple.dock expose-animation-duration -float 1; killall Dock;
+defaults write com.apple.dock expose-animation-duration -float 1; killall Dock; sleep 60; defaults write com.apple.dock expose-animation-duration -float 0.5;
